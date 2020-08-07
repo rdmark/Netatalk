6d99ad5a67b3594cc4e863ab9e04266fb1dc15aa
Christopher Kobayashi <software+android@disavowed.jp>
2020-08-07 20:33:44 +0900

    More AFS removal.

--- etc/afpd/afs.c
+++
@@ -1,8 +0,0 @@
-/*
- * $Id: afs.c,v 1.18 2009-10-15 10:43:13 didg Exp $
- * Copyright (c) 1990,1993 Regents of The University of Michigan.
- * All Rights Reserved.  See COPYRIGHT.
- */
-
-#include "config.h"
-
