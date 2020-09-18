a2dd1724860f82322a54bdbac15e350c7054fa3e
Christopher Kobayashi <software+android@disavowed.jp>
2020-09-18 20:13:41 +0900

    Remove krb4.

--- etc/uams/uams_krb4/kuam.c
+++
@@ -1,8 +0,0 @@
-/*
- * $Id: kuam.c,v 1.4 2001-06-25 20:13:45 rufustfirefly Exp $
- *
- * Copyright (c) 1990,1994 Regents of The University of Michigan.
- * All Rights Reserved.  See COPYRIGHT.
- */
-
-#include "config.h"
