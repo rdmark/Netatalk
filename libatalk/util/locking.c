00a23e245043fa2d276312181899837dd59e59e4
Christopher Kobayashi <software+android@disavowed.jp>
2020-09-23 11:16:04 +0900

    Indent.

--- libatalk/util/locking.c
+++ libatalk/util/locking.c
@@ -81,14 +81,15 @@
  *
  * @sa read_lock, write_lock, unlock
  */
-int lock_reg(int fd, int cmd, int type, off_t offset, int whence, off_t len)
+int lock_reg(int fd, int cmd, int type, off_t offset, int whence,
+	     off_t len)
 {
-    struct flock lock;
+	struct flock lock;
 
-    lock.l_type = type;
-    lock.l_start = offset;
-    lock.l_whence = whence;
-    lock.l_len = len;
+	lock.l_type = type;
+	lock.l_start = offset;
+	lock.l_whence = whence;
+	lock.l_len = len;
 
-    return (fcntl(fd, cmd, &lock));
+	return (fcntl(fd, cmd, &lock));
 }
--- libatalk/util/module.c
+++ libatalk/util/module.c
@@ -10,28 +10,28 @@
 
 #ifndef HAVE_DLFCN_H
 
-#else /* HAVE_DLFCN_H */
+#else				/* HAVE_DLFCN_H */
 
 #include <dlfcn.h>
 
 #ifdef DLSYM_PREPEND_UNDERSCORE
 void *mod_symbol(void *module, const char *name)
 {
-   void *symbol;
-   char *underscore;
+	void *symbol;
+	char *underscore;
 
-   if (!module)
-     return NULL;
+	if (!module)
+		return NULL;
 
-   if ((underscore = (char *) malloc(strlen(name) + 2)) == NULL)
-     return NULL;
+	if ((underscore = (char *) malloc(strlen(name) + 2)) == NULL)
+		return NULL;
 
-   strcpy(underscore, "_");
-   strcat(underscore, name);
-   symbol = dlsym(module, underscore);
-   free(underscore);
+	strcpy(underscore, "_");
+	strcat(underscore, name);
+	symbol = dlsym(module, underscore);
+	free(underscore);
 
-   return symbol;
+	return symbol;
 }
-#endif /* DLSYM_PREPEND_UNDERSCORE */
-#endif /* HAVE_DLFCN_H */
+#endif				/* DLSYM_PREPEND_UNDERSCORE */
+#endif				/* HAVE_DLFCN_H */
