9d83a49aa8054407ecc9b314bc4b12b90db4fa06
Christopher RYU <software+github@disavowed.jp>
2022-01-19 12:57:57 +0900

    Make it blindly obvious what this routine is doing.

--- etc/afpd/auth.c
+++ etc/afpd/auth.c
@@ -160,7 +160,9 @@ static int set_auth_switch(int expired)
 {
 	int i;
 
-	if (expired) {
+	if (!expired) {
+		afp_switch = postauth_switch;
+	} else {
 		/*
 		 * BF: expired password handling
 		 * to allow the user to change his/her password we have to allow login
