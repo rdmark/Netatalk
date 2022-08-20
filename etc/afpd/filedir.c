9fed3bf4cb3b663f36e744272205265338503ef7
Daniel Markstedt <markstedt@gmail.com>
2022-08-20 13:44:46 -0700

    Restore fix for error when moving file with shortened file name. This was fixed earlier in this fork, but got mistakenly reverted when merging another patch. https://github.com/Netatalk/Netatalk/issues/150

--- etc/afpd/filedir.c
+++ etc/afpd/filedir.c
@@ -726,8 +726,6 @@ int afp_moveandrename(AFPObj * obj, char *ibuf, size_t ibuflen _U_,
 			rc = AFPERR_PARAM;
 			goto exit;
 		}
-		curdir->d_offcnt++;
-		sdir->d_offcnt--;
 		/* if unix priv don't try to match perm with dest folder */
 		if (!isdir && !vol_unix_priv(vol)) {
 			int admode = ad_mode("", 0777) | vol->v_fperm;
