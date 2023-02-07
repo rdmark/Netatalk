4fe2dd14f837cab4c0dde139a3d8742e775d2742
Daniel Markstedt <markstedt@gmail.com>
2023-02-06 19:08:22 -0800

    Fall back to timezone when tm_gmtoff is unavailable

--- contrib/timelord/timelord.c
+++ contrib/timelord/timelord.c
@@ -232,14 +232,12 @@ int main( int ac, char **av )
             exit( 1 );
             }
 
-// TODO: Find a portable way to get time zone adjusted time
-// tm_gmtoff does not exist on Solaris with kin
 #if defined (HAVE_STRUCT_TM_GMTOFF)
-            mtime = tv.tv_sec + EPOCH + tm->tm_gmtoff;
-#else
-            mtime = tv.tv_sec + EPOCH;
-#endif
-            LOG(log_info, logtype_default, "Time zone is localtime" );
+			mtime = tv.tv_sec + EPOCH + tm->tm_gmtoff;
+#else /* HAVE_STRUCT_TM_GMTOFF */
+			mtime = tv.tv_sec + EPOCH - timezone;
+#endif /* HAVE_STRUCT_TM_GMTOFF */
+			LOG(log_info, logtype_default, "Time zone is localtime" );
         }
         else {
             if (( tm = gmtime( &tv.tv_sec )) == 0 ) {
