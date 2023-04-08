6e12ea1e8562591ffb7b51945cf5c032d3e671a2
dgsga <181612+dgsga@users.noreply.github.com>
2023-04-08 18:15:57 +0100

    Avoid setting adouble entries on symlinks
    Undo previous reversion of commit 3ba4dac

--- etc/afpd/file.c
+++ etc/afpd/file.c
@@ -984,7 +984,7 @@ int setfilparams(const AFPObj *obj, struct vol *vol,
     /* second try with adouble open
     */
     if (ad_open(adp, upath, ADFLAGS_HF | ADFLAGS_RDWR | ADFLAGS_CREATE, 0666) < 0) {
-        LOG(log_debug, logtype_afpd, "setfilparams: ad_open_metadata error");
+        LOG(log_debug, logtype_afpd, "setfilparams: ad_open_metadata error: %s", strerror(errno));
         /*
          * For some things, we don't need an adouble header:
          * - change of modification date
@@ -1016,6 +1016,9 @@ int setfilparams(const AFPObj *obj, struct vol *vol,
 
         switch(  bit ) {
         case FILPBIT_ATTR :
+            if (isad == 0) {
+                break;
+            }
             ad_getattr(adp, &bshort);
             oshort = bshort;
             if ( ntohs( ashort ) & ATTRBIT_SETCLR ) {
@@ -1029,14 +1032,23 @@ int setfilparams(const AFPObj *obj, struct vol *vol,
             ad_setattr(adp, bshort);
             break;
         case FILPBIT_CDATE :
+            if (isad == 0) {
+                break;
+            }
             ad_setdate(adp, AD_DATE_CREATE, cdate);
             break;
         case FILPBIT_MDATE :
             break;
         case FILPBIT_BDATE :
+            if (isad == 0) {
+                break;
+            }
             ad_setdate(adp, AD_DATE_BACKUP, bdate);
             break;
         case FILPBIT_FINFO :
+            if (isad == 0) {
+                break;
+            }
             if (!ad_entry(adp, ADEID_FINDERI)) {
                 LOG(log_debug, logtype_afpd, "setfilparams(\"%s\"): invalid FinderInfo", path->u_name);
                 break;
@@ -1062,6 +1074,9 @@ int setfilparams(const AFPObj *obj, struct vol *vol,
             }
             break;
         case FILPBIT_PDINFO :
+            if (isad == 0) {
+                break;
+            }
             ade = ad_entry(adp, ADEID_FINDERI);
             AFP_ASSERT(ade != NULL);
             if (obj->afp_version < 30) { /* else it's UTF8 name */
