dc5ecb7e7895038f59ef28cb2bf9cc4a7e420639
Daniel Markstedt <markstedt@gmail.com>
2023-07-03 17:35:07 -0700

    Install the libtool-bin package

--- contrib/shell_utils/debian_install.sh
+++ contrib/shell_utils/debian_install.sh
@@ -103,7 +103,7 @@ function installNetatalk() {
         echo
         echo "Installing dependencies..."
         sudo apt-get update || true
-        sudo apt-get install libssl-dev libdb-dev libcups2-dev cups libavahi-client-dev autotools-dev automake libtool libgcrypt20-dev pkg-config --assume-yes --no-install-recommends </dev/null
+        sudo apt-get install libssl-dev libdb-dev libcups2-dev cups libavahi-client-dev autotools-dev automake libtool libtool-bin libgcrypt20-dev pkg-config --assume-yes --no-install-recommends </dev/null
     fi
 
     cd "$BASE/../.." || exit 1
